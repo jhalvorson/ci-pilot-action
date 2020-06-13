efd6b7cd935e1c772f6446433c89342f82ecfd2c
Jamie Halvorson <j.halvorson@fairfx.com>
2020-06-13 13:58:37 +0100

    fix: pass context

--- src/list-commands.ts
+++ src/list-commands.ts
@@ -11,12 +11,12 @@ export const listCommands = async (
     const body =
       '### :wave: here are some helpful commands\n\n\n`ci-pilot deploy to staging`\nThis will tag your current branch with a staging command, allowing your CI to deploy to a staging environment.'
 
-    if (context.payload.pull_request?.number) {
+    if (context.issue.number) {
       await client.issues.createComment({
         ...context.repo,
         body,
         // eslint-disable-next-line @typescript-eslint/camelcase
-        issue_number: context.payload.pull_request.number
+        issue_number: context.issue.number
       })
     }
   } catch (err) {
