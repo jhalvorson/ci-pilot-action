20aa82805f1e603f5fd369a429df621b7bb22055
Jamie Halvorson <j.halvorson@fairfx.com>
2020-06-13 10:12:30 +0100

    feat: test action

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
 import * as core from '@actions/core'
 import {context} from '@actions/github'
+import {GitHub} from '@actions/github/lib/utils'
+
+const STAGING_DEPLOY_COMMENT = 'ci-pilot deploy to staging'
 
 async function run(): Promise<void> {
   try {
     // You can use the isDevMode flag to skip certain checks such as branch
     // name validation
-    const isDevMode: boolean = core.getInput('devMode') === 'true'
+    const isDevMode: boolean = core.getInput('dev_mode') === 'true'
 
     // Ensure that we're on the release branch, if we're not on the release
     // branch then immediately fail the job and provide some basic feedback
@@ -14,6 +17,24 @@ async function run(): Promise<void> {
       core.setFailed(
         `A deployment to staging was triggered from ${context.ref}. Staging deployments may only be triggered from release branches.`
       )
+    }
+
+    const client = new GitHub()
+    const comment =
+      context.eventName === 'issue_comment'
+        ? context.payload.comment?.body
+        : null
+    const {owner, repo} = context.repo
+
+    if (comment === STAGING_DEPLOY_COMMENT && context.payload.comment?.id) {
+      // React to the comment to acknowledge that we've tagged the branch
+      await client.reactions.createForIssueComment({
+        owner,
+        repo,
+        // eslint-disable-next-line @typescript-eslint/camelcase
+        comment_id: context.payload.comment.id,
+        content: '+1'
+      })
     }
   } catch (error) {
     // The action has failed, use built in error handling
