45b91db861509eddf414ed5f258768bc44a5edca
Jamie Halvorson <j.halvorson@fairfx.com>
2020-06-13 09:21:08 +0100

    chore: formatting

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import * as core from '@actions/core'
-import { context } from '@actions/github';
+import {context} from '@actions/github'
 
 async function run(): Promise<void> {
   try {
@@ -7,9 +7,10 @@ async function run(): Promise<void> {
     // branch then immediately kill the job and provide some basic feedback
     const currentBranch = context.ref
     if (!currentBranch.includes('release/')) {
-      core.setFailed(`A deployment to staging was triggered from ${context.ref}. Staging deployments may only be triggered from release branches.`)
+      core.setFailed(
+        `A deployment to staging was triggered from ${context.ref}. Staging deployments may only be triggered from release branches.`
+      )
     }
-    
   } catch (error) {
     // The action has failed, use built in error handling
     core.setFailed(error.message)
