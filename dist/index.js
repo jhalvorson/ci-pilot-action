d8d446d9f25c8565ddb1505a9fd5311920f5e918
Jamie Halvorson <j.halvorson@fairfx.com>
2020-06-13 14:11:35 +0100

    feat: update help message

--- dist/index.js
+++ dist/index.js
@@ -9120,7 +9120,12 @@ const core = __importStar(__webpack_require__(470));
 exports.listCommands = (client, context) => __awaiter(void 0, void 0, void 0, function* () {
     try {
         console.log('running help command');
-        const body = '### :wave: CI Pilot here :woman_pilot:\n\n\n\nYou can use the following commands to control ci-pilot:\n\n\n`ci-pilot deploy to staging`\n\nThis will tag your current branch with a staging command, allowing your CI to deploy to a staging environment.';
+        const exampleTag = `staging-${new Date().getTime()}`;
+        const body = `### :wave: hello\n\n\n\n
+      You can use the following commands to control ci-pilot:\n\n\n
+      \`ci-pilot deploy to staging\`\n\n
+      This will tag your current branch with a staging tag (e.g ${exampleTag}), allowing your CI to deploy to a staging environment.
+    `;
         if (context.issue.number) {
             yield client.issues.createComment(Object.assign(Object.assign({}, context.repo), { body, issue_number: context.issue.number }));
         }
